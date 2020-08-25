bd59f3260aa392fd383d2b088fa9aa2bb6488cd1
Vudang <vudang@Vudangs-iMac.local>
2020-08-25 22:41:12 +0700

    Done Map

--- src/screens/MapTheaterScreen.js
+++ src/screens/MapTheaterScreen.js
@@ -1,17 +1,40 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-import MapView from 'react-native-maps'
+import { View, Text,SafeAreaView } from 'react-native'
+import MapView, { Circle } from 'react-native-maps'
 
-export default function MapTheaterScreen() {
+import Ant from 'react-native-vector-icons/AntDesign'
+
+export default function MapTheaterScreen({route,navigation}) {
+  const {lat,lng} = route.params.coors;
+  const coors = {
+    latitude: lat,
+    longitude: lng,
+  }
   return (
-    
+    <SafeAreaView style={{flex:1,backgroundColor:'#4976d6',opacity:0.85}}>
+    <View style={{flex:1}}>
+    <Ant onPress={() => navigation.goBack()} style={{zIndex:99,left:30}} name='arrowleft' size={40} color='white'/>
+    <Text style={{fontSize:30,fontWeight:'bold',color:'black',textAlign:'center',bottom:40}}>MAP</Text>
+    </View>
     <MapView
-    style={{flex:1}}
+    style={{flex:9}}
     initialRegion={{
-      latitude:10.780503,
-      longitude: 106.638376,
+      latitude:lat,
+      longitude: lng,
       latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421}}/>
+      longitudeDelta: 0.0421}}
+    
+    >
+      <Circle
+        center={coors}
+        radius={1000}
+        strokeColor='red'
+      />
+    </MapView>
+    
+    
+    </SafeAreaView>
+    
    
   )
 }
